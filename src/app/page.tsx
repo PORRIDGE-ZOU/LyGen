bd95a62c6edb3244f1614a7a37d695b62afb4454
Haoxuan Zou <porridgeorge@gmail.com>
2024-08-01 11:13:33 -0400

    First - canvas setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"; // next.js app router
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import * as fabric from "fabric";
+import { FabricObject } from "fabric";
+
+const App = () => {
+  const [canvas, setCanvas] = useState<fabric.Canvas>();
+
+  useEffect(() => {
+    const c = new fabric.Canvas("canvas", {
+      height: 540,
+      width: 960,
+      backgroundColor: "black",
+    });
+
+    // settings for all canvas in the app
+    FabricObject.prototype.transparentCorners = false;
+    FabricObject.prototype.cornerColor = "#2BEBC8";
+    FabricObject.prototype.cornerStyle = "rect";
+    FabricObject.prototype.cornerStrokeColor = "#2BEBC8";
+    FabricObject.prototype.cornerSize = 6;
+
+    c.renderAll();
+    setCanvas(c);
+
+    return () => {
+      console.log("[App] canvas cleaning up");
+      c.dispose();
+    };
+  }, []);
+
+  const addRect = (canvas?: fabric.Canvas) => {
+    const rect = new fabric.Rect({
+      height: 280,
+      width: 200,
+      stroke: "#2BEBC8",
+    });
+    canvas?.add(rect);
+    canvas?.requestRenderAll();
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <button onClick={() => addRect(canvas)}>Rectangle</button>
+      <canvas id="canvas" />
+    </div>
   );
-}
+};
+
+export default App;
